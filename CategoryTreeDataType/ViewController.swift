2af6749523ab0742506abfae14545495b92d6306
Asrbek <asrbekmahmudov>
2022-01-12 09:55:12 +0500

    Initial Commit

---
+++ CategoryTreeDataType/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CategoryTreeDataType
+//
+//  Created by Mahmudov Asrbek Ulug'bek o'g'li on 12/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
